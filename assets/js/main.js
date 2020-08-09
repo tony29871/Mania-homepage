87b4986b8c3fb249b648c8fceddb6c2b079ab54d
tony29871 <tony29871@naver.com>
2020-08-09 14:39:28 +0900

    모바일 가로 화면 가능하게 수정(세로 불가)

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,179 +6,192 @@
 
 (function($) {
 
-	skel.breakpoints({
-		xlarge:	'(max-width: 1680px)',
-		large:	'(max-width: 1280px)',
-		medium:	'(max-width: 980px)',
-		small:	'(max-width: 736px)',
-		xsmall:	'(max-width: 480px)'
-	});
+    skel.breakpoints({
+        xlarge: '(max-width: 1680px)',
+        large: '(max-width: 1280px)',
+        medium: '(max-width: 980px)',
+        small: '(max-width: 736px)',
+        xsmall: '(max-width: 480px)'
+    });
 
-	$(function() {
+    $(function() {
 
-		var	$window = $(window),
-			$body = $('body'),
-			$header = $('#header'),
-			$banner = $('#banner');
+        var $window = $(window),
+            $body = $('body'),
+            $header = $('#header'),
+            $banner = $('#banner');
 
-		var $height = $('#header').height();
+        var $height = $('#header').height();
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+        // Disable animations/transitions until the page has loaded.
+        $body.addClass('is-loading');
 
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
+        $window.on('load', function() {
+            window.setTimeout(function() {
+                $body.removeClass('is-loading');
+            }, 100);
+        });
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+        // Fix: Placeholder polyfill.
+        $('form').placeholder();
 
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
+        // Prioritize "important" elements on medium.
+        skel.on('+medium -medium', function() {
+            $.prioritize(
+                '.important\\28 medium\\29',
+                skel.breakpoint('medium').active
+            );
+        });
 
-		// Banner
+        // Banner
 
-			if ($banner.length > 0) {
+        if ($banner.length > 0) {
 
-				// IE: Height fix.
-					if (skel.vars.browser == 'ie'
-					&&	skel.vars.IEVersion > 9) {
+            // IE: Height fix.
+            if (skel.vars.browser == 'ie' &&
+                skel.vars.IEVersion > 9) {
 
-						skel.on('-small !small', function() {
-							$banner.css('height', '100vh');
-						});
+                skel.on('-small !small', function() {
+                    $banner.css('height', '100vh');
+                });
 
-						skel.on('+small', function() {
-							$banner.css('height', '');
-						});
+                skel.on('+small', function() {
+                    $banner.css('height', '');
+                });
 
-					}
+            }
 
-				// More button.
-					$banner.find('.more')
-						.addClass('scrolly');
+            // More button.
+            $banner.find('.more')
+                .addClass('scrolly');
 
-			}
-
-
-		// Get BG Image
-
-			if ( $( ".bg-img" ).length ) {
-
-				$( ".bg-img" ).each(function() {
-
-					var post 	= $(this),
-						bg 		= post.data('bg');
-
-					post.css( 'background-image', 'url(images/' + bg + ')' );
-
-				});
+        }
 
 
-			}
+        // Get BG Image
 
-		// Posts
+        if ($(".bg-img").length) {
 
-			$( ".post" ).each( function() {
-				var p = $(this),
-					i = p.find('.inner'),
-					m = p.find('.more');
+            $(".bg-img").each(function() {
 
-				m.addClass('scrolly');
+                var post = $(this),
+                    bg = post.data('bg');
 
-				p.scrollex({
-					top: '40vh',
-					bottom: '40vh',
-					terminate: 	function() { m.removeClass('current'); i.removeClass('current'); },
-					enter: 		function() { m.addClass('current'); i.addClass('current'); },
-					leave: 		function() { m.removeClass('current'); i.removeClass('current'); }
-				});
+                post.css('background-image', 'url(images/' + bg + ')');
 
-			});
-
-		// Scrolly.
-			if ( $( ".scrolly" ).length ) {
-
-				$('.scrolly').scrolly();
-			}
-
-		// Menu.
-			$('#menu')
-				.append('<a href="#menu" class="close"></a>')
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'right'
-				});
-
-	});
-
-			// Gallery.
-			$('.gallery').each(function() {
-
-				var	$gallery = $(this),
-					$content = $gallery.find('.content');
-
-				// Poptrox.
-					$content.poptrox({
-						usePopupCaption: true
-					});
-
-				// Tabs.
-					$gallery.each( function() {
-
-						var $this = $(this),
-							$tabs = $this.find('.tabs a'),
-							$media = $this.find('.media');
-
-						$tabs.on('click', function(e) {
-
-							var $this = $(this),
-								tag = $this.data('tag');
-
-							// Prevent default.
-							 	e.preventDefault();
-
-							// Remove active class from all tabs.
-								$tabs.removeClass('active');
-
-							// Reapply active class to current tab.
-								$this.addClass('active');
-
-							// Hide media that do not have the same class as the clicked tab.
-								$media
-									.fadeOut('fast')
-									.each(function() {
-
-										var $this = $(this);
-
-										if ($this.hasClass(tag))
-											$this
-												.fadeOut('fast')
-												.delay(200)
-												.queue(function(next) {
-													$this.fadeIn();
-													next();
-												});
-
-									});
-
-						});
-
-					});
+            });
 
 
-			});
+        }
+
+        // Posts
+
+        $(".post").each(function() {
+            var p = $(this),
+                i = p.find('.inner'),
+                m = p.find('.more');
+
+            m.addClass('scrolly');
+
+            p.scrollex({
+                top: '40vh',
+                bottom: '40vh',
+                terminate: function() {
+                    m.removeClass('current');
+                    i.removeClass('current');
+                },
+                enter: function() {
+                    m.addClass('current');
+                    i.addClass('current');
+                },
+                leave: function() {
+                    m.removeClass('current');
+                    i.removeClass('current');
+                }
+            });
+
+        });
+
+        // Scrolly.
+        if ($(".scrolly").length) {
+
+            $('.scrolly').scrolly();
+        }
+
+        // Menu.
+        $('#menu')
+            .append('<a href="#menu" class="close"></a>')
+            .appendTo($body)
+            .panel({
+                delay: 500,
+                hideOnClick: true,
+                hideOnSwipe: true,
+                resetScroll: true,
+                resetForms: true,
+                side: 'right'
+            });
+
+    });
+
+    // Gallery.
+    $('.gallery').each(function() {
+
+        var $gallery = $(this),
+            $content = $gallery.find('.content');
+
+        // Poptrox.
+        $content.poptrox({
+            usePopupCaption: true
+        });
+
+        // Tabs.
+        $gallery.each(function() {
+
+            var $this = $(this),
+                $tabs = $this.find('.tabs a'),
+                $media = $this.find('.media');
+
+            $tabs.on('click', function(e) {
+
+                var $this = $(this),
+                    tag = $this.data('tag');
+
+                // Prevent default.
+                e.preventDefault();
+
+                // Remove active class from all tabs.
+                $tabs.removeClass('active');
+
+                // Reapply active class to current tab.
+                $this.addClass('active');
+
+                // Hide media that do not have the same class as the clicked tab.
+                $media
+                    .fadeOut('fast')
+                    .each(function() {
+
+                        var $this = $(this);
+
+                        if ($this.hasClass(tag))
+                            $this
+                            .fadeOut('fast')
+                            .delay(200)
+                            .queue(function(next) {
+                                $this.fadeIn();
+                                next();
+                            });
+
+                    });
+
+            });
+
+        });
+
+
+    });
 
 })(jQuery);
+
+
+var time = document.querySelectorAll(".timebox img")
+console.log(time)
